9a362e026d07d9f502e898b2ffe71d4021b490bb
pravtz <ederson.ufrgs@gmail.com>
2021-11-19 08:50:54 -0300

    settings styles-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import Head from 'next/head'
+import GlobalStyled from '../styles/global'
+import defaultTheme from '../styles/theme/default'
+import NextProgress from 'next-progress'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="favicon.svg" />
+      </Head>
+
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyled />
+        <NextProgress color="#6b705c" height={5} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
