bbed9ef2025a5b8a4ce5c527a3179b85eb02d2cf
pravtz <ederson.ufrgs@gmail.com>
2021-11-19 08:55:13 -0300

    create pages start static

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,11 @@
-import Layout from "../components/Layout";
-import { attributes, html } from "../../content/about.md";
+import Layout from '../components/Layout'
+import { attributes, html } from '../../content/about.md'
 
 const About = () => (
   <Layout>
     <h1>{attributes.title}</h1>
     <div dangerouslySetInnerHTML={{ __html: html }} />
   </Layout>
-);
+)
 
-export default About;
+export default About
