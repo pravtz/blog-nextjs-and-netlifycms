91f1db7616c499ec272fb89af4f9c695d7c10154
pravtz <ederson.ufrgs@gmail.com>
2021-11-22 23:57:54 -0300

    set config fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line no-undef
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack: (configuration) => {
     configuration.module.rules.push({
       test: /\.md$/,
-      use: 'frontmatter-markdown-loader',
+      use: 'frontmatter-markdown-loader'
     })
+    // if (!isServer) {
+    //   // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+    //   configuration.resolve.fallback = {
+    //     fs: false
+    //   }
+    // }
+
     return configuration
   }
 }
