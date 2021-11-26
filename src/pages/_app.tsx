98dc963c58f469016c77e045c8a74e4b324524a0
pravtz <ederson.ufrgs@gmail.com>
2021-11-26 17:20:14 -0300

    add font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
         <link rel="apple-touch-icon" href="favicon.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;500;700&family=Ubuntu:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <ThemeProvider theme={defaultTheme}>
