5b8ac5d849d3dd01ceb55d3d8b303cd75380656b
pravtz <ederson.ufrgs@gmail.com>
2021-12-02 23:41:36 -0300

    fix Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,16 @@
-import { Wrapper } from './styled'
+import Footer from '../Footer'
+import HeaderBar from '../HeaderBar'
+import { Wrapper, Content } from './styled'
 
 type LayoutProps = {
   children: React.ReactNode
 }
-const Layout = ({ children }: LayoutProps) => <Wrapper>{children}</Wrapper>
+const Layout = ({ children }: LayoutProps) => (
+  <Wrapper>
+    <HeaderBar />
+    <Content>{children}</Content>
+    <Footer />
+  </Wrapper>
+)
 
 export default Layout
--- src/components/Layout/styled.ts
+++ src/components/Layout/styled.ts
@@ -1,6 +1,28 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main`
-  background-color: ${(props) => props.theme.colors.primary};
+export const Wrapper = styled.div`
+  background-color: white;
+  //background-color: ${(props) => props.theme.colors.primary};
   height: 100%;
+`
+export const Content = styled.div`
+  width: 100vw;
+  ${media.greaterThan('medium')`
+    /* screen width is greater than 768px (medium) */
+    width: 767px;
+    margin: auto;
+  `}
+  ${media.greaterThan('large')`
+    /* screen width is greater than 1170px (large) */
+    width: 1169px;
+    margin: auto;
+
+  `}
+  ${media.greaterThan('huge')`
+    /* screen width is greater than 1440px (huge) */
+    width: 1439px;
+    margin: auto;
+
+  `}
 `
