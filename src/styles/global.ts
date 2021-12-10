a09efbd230c12ee409d241c953c9a3d8516ded00
pravtz <ederson.ufrgs@gmail.com>
2021-12-09 23:27:35 -0300

    add styles global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,15 +6,28 @@ const GlobalStyled = createGlobalStyle`
   padding: 0;
   margin: 0;
 }
+a{
+  cursor: pointer;
+}
   html, body, #__next {
     height: 100%;
   }
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+
+  p, a, address{
+    font-family: 'Ubuntu', sans-serif;
+
   }
+  h1,h2,h3,h4,h5,h6{
+    font-family: 'Montserrat', sans-serif;
+}
   /* html {
     font-size: 62.5%
   } */
+:root{
+  --spacing-betwen-box-align: 2.5rem; //40px
+  --spacing-basic: 0.3125rem; //5px
+  --font-size-title: 1.375rem; //22px
+}
+
 `
 export default GlobalStyled
