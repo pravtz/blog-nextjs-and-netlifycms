153709cdfbef10c672c226e3854e7e9c6dfdfcbd
pravtz <ederson.ufrgs@gmail.com>
2021-12-09 23:28:01 -0300

    add styles theme

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,11 +1,19 @@
 const defaultTheme = {
+  nameTheme: 'default',
   colors: {
     primary: '#cb997e',
     secondary: '#ddbea9',
     text: '#ffe8d6',
     tertiary: '#b7b7a4',
     bgTwo: '#a5a58d',
-    bg: '#6b705c'
+    bg: '#6b705c',
+
+    // styles color originals project
+    colorMediumGray: '#666666',
+    colorLightGray: '#b9b9b9',
+
+    colorIcons: '#666',
+    colorIconsHover: '#111'
   }
 }
 
