5d3ef2acb67d3650c87f89f1e5438d2af06b2c90
pravtz <ederson.ufrgs@gmail.com>
2021-12-17 11:37:30 -0300

    fix styled headerBar

--- src/components/HeaderBar/styled.ts
+++ src/components/HeaderBar/styled.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import media from 'styled-media-query'
 
 export const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.colors.backgraundHeaderBar};
   max-width: 100vw;
   border-bottom: 1px solid silver;
 
@@ -75,10 +76,11 @@ export const NavBar = styled.div`
 `
 
 export const ButtonResponsive = styled.div`
+  margin-right: 16px;
   button {
     outline: none;
     border: none;
-    background-color: white;
+    background-color: ${(props) => props.theme.colors.backgraundHeaderBar};
     color: #666;
     &:hover {
       color: black;
@@ -88,8 +90,39 @@ export const ButtonResponsive = styled.div`
   display: none;
 `}
 `
-export const Brand = styled.div``
+export const Brand = styled.div`
+  margin-left: 16px;
+  ${media.greaterThan('small')`
+    /* screen width is greater than 768px (medium) */
+    h1:after{
+      content: ' - small';
+      font-size: 14px;
+    }
+  `}
+  ${media.greaterThan('medium')`
+    /* screen width is greater than 768px (medium) */
+    h1:after{
+      content: ' - medium';
+      font-size: 14px;
+    }
+  `}
+  ${media.greaterThan('large')`
+    /* screen width is greater than 1170px (large) */
+    h1:after{
+      content: ' - large';
+      font-size: 14px;
+    }
+  `}
+  ${media.greaterThan('huge')`
+    /* screen width is greater than 1440px (huge) */
+    h1:after{
+      content: ' - huge';
+      font-size: 14px;
+    }
+  `}
+`
 export const SocialMedia = styled.div`
+  margin-right: 16px;
   display: none;
   ${media.greaterThan('medium')`
     display: block;
