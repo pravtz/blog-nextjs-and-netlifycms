d6f54dd927492f286c2f1ff40ec6e66f197400ba
pravtz <ederson.ufrgs@gmail.com>
2022-01-09 14:38:23 -0300

    redefining the method crossOrigin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
         <link rel="apple-touch-icon" href="favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;500;700&family=Ubuntu:wght@300;400;700&display=swap"
           rel="stylesheet"
