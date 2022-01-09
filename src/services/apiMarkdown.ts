def7e02b99cd0fec19be0f0c85fcb2a6df357ed2
pravtz <ederson.ufrgs@gmail.com>
2022-01-09 14:36:38 -0300

    retructuring of the function ListPost

--- src/services/apiMarkdown.ts
+++ src/services/apiMarkdown.ts
@@ -1,3 +1,5 @@
+import { ListPostPromisse } from '../types/type'
+
 export const importBlogPosts = async () => {
   const markdownFiles = require
     .context('../../content/blog', false, /\.md$/)
@@ -14,11 +16,34 @@ export const importBlogPosts = async () => {
     })
   )
 }
-export const slugs = async () => {
+export const ListPost = async () => {
   const slugFiles = require
     .context('../../content/blog', false, /\.md$/)
     .keys()
     .map((relativePath: string) => relativePath.substring(2))
 
-  return slugFiles
+  return Promise.all<ListPostPromisse>(
+    slugFiles.map(async (path: string) => {
+      const markdown = await import(`../../content/blog/${path}`)
+
+      // const e = l.filter((name) => {
+      //   return name === 'Tecnologia'
+      // })
+
+      const r = {
+        ...markdown.attributes,
+        slug: path.substring(0, path.length - 3)
+      }
+
+      return r
+    })
+  )
+}
+
+export const categoryPosts = async (
+  category: string
+): Promise<ListPostPromisse[]> => {
+  const list = await ListPost()
+
+  return list.filter((post) => post.category == category)
 }
